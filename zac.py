227dd05c8a16a411057012e804edb456857f1510
zac <zaccoder@gmail.com>
2013-11-26 14:51:19 +0800

    zac

--- zac.py
+++ zac.py
@@ -53,27 +53,29 @@ if __name__ == '__main__':
     except getopt.GetoptError:
         print "input worng,the good Example: python zac.py -n 10 -x 6 -y 5"
         sys.exit(1)
+    try:
+        for opt, value in opts:
+            if opt == "-n":
+                n = int(value)
+                if n < 2:
+                    print "n must be greater than 2"
+                    sys.exit(1)
+            elif opt == "-x":
+                x = int(value)
+                if x > n:
+                    print "x must be less than n"
+                    sys.exit(1)
+            elif opt == "-y":
+                y = int(value)
+                if y > n:
+                    print "y must be less than y"
+                    sys.exit(1)
 
-    for opt, value in opts:
-        if opt == "-n":
-            n = int(value)
-            if n < 2:
-                print "n must be greater than 2"
-                sys.exit(1)
-        elif opt == "-x":
-            x = int(value)
-            if x > n:
-                print "x must be less than n"
-                sys.exit(1)
-        elif opt == "-y":
-            y = int(value)
-            if y > n:
-                print "y must be less than y"
-                sys.exit(1)
-
-    result = go(n, Point(x, y), Point(n - 1, n - 1))
-    if result:
-        for point in result.path:
-            print "({0.x},{0.y})".format(point)
-    else:
-        print "no way!"
+        result = go(n, Point(x, y), Point(n - 1, n - 1))
+        if result:
+            for point in result.path:
+                print "({0.x},{0.y})".format(point)
+        else:
+            print "no way!"
+    except NameError:
+        print "you must input the value of n, x, y"
