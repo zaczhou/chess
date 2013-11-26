f06e3785cb7047d03dda15e54d98779283036fd9
zac <zaccoder@gmail.com>
2013-11-26 14:09:18 +0800

    zac

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+from zac import Point, go
+
+class ZacTest(unittest.TestCase):
+    """
+    test for go method
+    """
+
+    def test_point(self):
+        self.assertEqual(Point(0,0).path, [])
+    
+    def test_go(self):
+        """
+        test go method
+        """
+        route = [(p.x, p.y) for p in go(10, Point(0, 0), Point(9, 9)).path]
+        self.assertEqual(route, [(1, 2), (2, 4), (3, 6), (5, 7), (7, 8), (9, 9)])
+
+unittest.main()
--- zac.py
+++ zac.py
@@ -2,61 +2,73 @@
 # encoding: utf-8
 
 """
-Author: zac 
+Author: zac
 """
 
 import sys
 import getopt
 
+
 class Point(object):
 
-    def __init__(self, x, y, path= []):
+    def __init__(self, x, y, path=[]):
         """
-        Point object for knight_move
-
-        :x: point x
-        :y: point y
-        :step: current step
-        :returns: Point object
-
+        x: point x
+        y: point y
+        path: the way go the point
         """
         self.x = int(x)
         self.y = int(y)
-        self.path = path
+        self.path = path # process
+
 
 def go(n, start_point, end_point):
-    
     n = int(n)
-    board = [[0 for tmp_y in xrange(n)] for tmp_x in xrange(n)]
+    matrix = [[0 for a in range(n)] for b in range(n)] # use to set where has been to 
     routes = [start_point]
-    rules = ((1, 2), (2, 1),(1, -2), (2, -1),(-1, -2), (-2, -1), (-2, 1), (-1, 2))
+    rules = ((1, 2), (2, 1), (1, -2), (2, -1),
+             (-1, -2), (-2, -1), (-2, 1), (-1, 2)) # go way
+    #m = 0
     while routes:
-        bre_point = routes.pop(0)
+        bre_point = routes.pop(0) # get bre_point to test go to end_point
         if bre_point.x == end_point.x and bre_point.y == end_point.y:
+            #print m
             return bre_point
-
+      
         for step in rules:
-            next_point = Point(bre_point.x + step[0], bre_point.y + step[1], [])
-            if (0 <= next_point.x < n and 0 <= next_point.y < n and not board[next_point.y][next_point.x]):
+            #m+= 1
+            next_point = Point(bre_point.x + step[0],
+                               bre_point.y + step[1], [])
+            if (0 <= next_point.x < n and 0 <= next_point.y < n
+                    and not matrix[next_point.x][next_point.y]):
+                # if point in matrix and did not to go , that is a odds , append it to routes
                 next_point.path = bre_point.path + [next_point]
                 routes.append(next_point)
-                board[next_point.y][next_point.x] = 1
-                print board
+                matrix[next_point.x][next_point.y] = 1 # set 
 
 if __name__ == '__main__':
     try:
         opts = getopt.getopt(sys.argv[1:], "n:x:y:")[0]
     except getopt.GetoptError:
-        print "you input in a worng way , the good Example: python zac.py -n 10 -x 6 -y 5 "
+        print "input worng,the good Example: python zac.py -n 10 -x 6 -y 5"
         sys.exit(1)
 
     for opt, value in opts:
         if opt == "-n":
             n = int(value)
+            if n < 2:
+                print "n must be greater than 2"
+                sys.exit(1)
         elif opt == "-x":
             x = int(value)
+            if x > n:
+                print "x must be less than n"
+                sys.exit(1)
         elif opt == "-y":
             y = int(value)
+            if y > n:
+                print "y must be less than y"
+                sys.exit(1)
 
     result = go(n, Point(x, y), Point(n - 1, n - 1))
     if result:
